0842edce0bb2b67003da31b2cf76e158f44d8689
h-tko <bojan_krkic_esp@i.softbank.jp>
2016-12-13 06:07:25 +0000

    postgres接続部分追加。formhelper追加

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ $ go get github.com/astaxie/beego
 $ go get github.com/beego/bee
 $ go get github.com/astaxie/beego/cache
 $ go get github.com/astaxie/beego/cache/redis
+$ go get github.com/astaxie/beego/orm
+$ go get github.com/lib/pq
 ```
 
 上記の通り、cacheとしてredisを使う想定です。
@@ -16,3 +18,7 @@ $ go get github.com/astaxie/beego/cache/redis
 libraries/cache.goにキャッシュ接続部分は実装してあります。
 
 また、redisへの接続設定周りは、conf/app.confにcache_として切ってあります。
+
+## ヘルパー関数
+
+独自にhelpersディレクトリを切ってあり、form_helpers.goを用意しているので、フォームヘルパーはここに追記お願いします
--- main.go
+++ main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
-	_ "derby-mas-memo/routers"
-	"github.com/astaxie/beego"
+    "fmt"
+    _ "derby-mas-memo/routers"
+    "github.com/astaxie/beego"
+    "github.com/astaxie/beego/orm"
+    _ "github.com/lib/pq"
 )
 
-func main() {
-	beego.Run()
+func init() {
+    initDatabase()
+    initTemplate()
 }
 
+func main() {
+    beego.Run()
+}
+
+func initDatabase() {
+    driver := beego.AppConfig.String("db_driver")
+    dbLink := fmt.Sprintf(
+        "user=%s dbname=%s sslmode=disable",
+        beego.AppConfig.String("db_user"),
+        beego.AppConfig.String("db_name"),
+    )
+
+    orm.RegisterDriver(driver, orm.DRPostgres)
+    orm.RegisterDataBase("default", driver, dbLink)
+}
+
+func initTemplate() {
+    helpers.RegistFormHelper()
+}
