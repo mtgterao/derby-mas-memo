8261af9e534c779b5c82bdef874e8ed8e5c8d1ef
h-tko <bojan_krkic_esp@i.softbank.jp>
2016-12-13 02:34:22 +0000

    first commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "derby-mas-memo/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, 'static/js/app.js'),
+    output: {
+        path: path.join(__dirname, 'static/dist'),
+        filename: 'bundle.js'
+    },
+    cache: true,
+    module: {
+        loaders: [{
+            test: /\.css$/, loader: 'style-loader!css-loader'
+        }, {
+            test: /\/js[x]?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+            query: {
+                presets: ['react', 'es2015']
+            }
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    devtool: 'source-map'
+}
